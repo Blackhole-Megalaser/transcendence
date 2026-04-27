2c46498d6cf6cbffa9264de07d5a56e302efd76e
Khaïs COLIN <khais.colin@gmail.com>
2026-04-27 16:42:15 +0200

    login working!

--- back/views.py
+++ back/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def index(request):
+    user = request.user
+    print(user)
+    return render(
+        request,
+        "back/index.html",
+        { "user": user }
+    )
+
+@login_required
+def profile(request):
+    user = request.user
+    return render(
+        request,
+        "back/profile.html",
+        { "user": user },
+    )
