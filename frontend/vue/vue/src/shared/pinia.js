729163f76d9888b190efced0c533edc6f637cba3
Sellith <leanevanbree@gmail.com>
2026-05-22 17:54:57 +0200

    Update: Added the login and signUp pages and changed some stuff with the navBar

--- frontend/vue/vue/src/shared/pinia.js
+++
@@ -1,8 +0,0 @@
-import { createPinia } 				from "pinia";
-import piniaPluginPersistedstate 	from 'pinia-plugin-persistedstate';
-
-export function setupPinia () {
-  const pinia = createPinia();
-  pinia.use(piniaPluginPersistedstate);
-  return (pinia);
-}
