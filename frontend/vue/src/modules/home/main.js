80ae445fc86f671f698ed69a714443badd7bac9e
Sellith <leanevanbree@gmail.com>
2026-05-23 16:38:26 +0200

    Cleanup: added a index file to include in shared to clean the script setup section

--- frontend/vue/src/modules/home/main.js
+++ frontend/vue/src/modules/home/main.js
@@ -1,15 +1,26 @@
-import { createApp } 		from 'vue';
-import { setupPinia } 		from '@shared/pinia';
-import HomeApp 				from './HomeApp.vue';
+import { createApp } 		  from 'vue';
+import HomeApp 				    from './HomeApp.vue';
 import { useThemeStore } 	from '@storage/theme';
 
-const pinia = setupPinia();
+import { 
+  setupPinia,
+  Button,
+  NavBar,
+  SideBar,
+  SideProfile
+} from '@shared';
+
 const app = createApp(HomeApp);
+const pinia = setupPinia();
 app.use(pinia);
 
 const savedTheme = useThemeStore();
 document.documentElement.setAttribute("data-theme", savedTheme.current);
 
+app.component('Button', Button);
+app.component('NavBar', NavBar);
+app.component('SideBar', SideBar);
+app.component('SideProfile', SideProfile);
 
 app.mount('#app')
 
