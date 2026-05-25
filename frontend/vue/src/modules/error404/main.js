28b4d500b4b3550d45a2bac5f4e4ff47543d041a
Sellith <leanevanbree@gmail.com>
2026-05-26 01:12:38 +0200

    Update: changed html files title to be more accurate on their purpose

---
+++ frontend/vue/src/modules/error404/main.js
@@ -0,0 +1,16 @@
+import { createApp }      from 'vue';
+import { useThemeStore }  from '@storage/theme';
+import { setupPinia }     from '@shared';
+import BasePage           from '@components/BasePage.vue';
+import App                from './App.vue';
+
+const app = createApp(App);
+const pinia = setupPinia();
+app.use(pinia);
+
+const savedTheme = useThemeStore();
+document.documentElement.setAttribute("data-theme", savedTheme.current);
+
+app.component('BasePage', BasePage);
+
+app.mount('#app')
