215c399c40a4e7cd232acb79efe8eadf29a531eb
LaFougere <timothe.lair@lafougere.dev>
2026-05-26 17:25:38 +0200

    Fix: forgot better formatting

--- frontend/vue/src/shared/fontawesome.js
+++ frontend/vue/src/shared/fontawesome.js
@@ -4,10 +4,26 @@ import { far }             from '@fortawesome/free-regular-svg-icons';
 import { fas }             from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+function mapIconsByName(pack) {
+  return Object.fromEntries(
+    Object.values(pack)
+      .filter((icon) => icon && typeof icon === 'object' && icon.iconName)
+      .map((icon) => [icon.iconName, icon])
+  );
+}
+
+export const byPrefixAndName = {
+  fas: mapIconsByName(fas),
+  far: mapIconsByName(far),
+  fab: mapIconsByName(fab),
+};
+
 library.add(fas, far, fab);
 
 export function setupFontAwesome(app) {
+  app.component('FontAwesomeIcon', FontAwesomeIcon);
   app.component('font-awesome-icon', FontAwesomeIcon);
+  app.config.globalProperties.byPrefixAndName = byPrefixAndName;
 }
 
 // FontAwesome icon usage
@@ -16,3 +32,4 @@ export function setupFontAwesome(app) {
 // then call setupFontAwesome(app)
 // In a Vue component:
 // 	<font-awesome-icon :icon="['fas', 'house']" />.
+// 	<FontAwesomeIcon :icon="byPrefixAndName.fas['house']" />.
