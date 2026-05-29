5c8ab838b5035543e20fa3331a0685d2036f42ad
Sellith <leanevanbree@gmail.com>
2026-05-29 15:08:23 +0200

    Update: Added svg images for subpage selection on the side profile

--- frontend/vue/src/components/sideProfile/Social.vue
+++ frontend/vue/src/components/sideProfile/Social.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="w-full relative">
-    <div class="w-full h-auto bg-button-2-normal flex justify-center items-stretch rounded-t-2xl overflow-hidden">
+    <div class="w-full h-auto bg-button-1-normal flex justify-center items-stretch rounded-t-2xl overflow-hidden font-semibold text-text-button-1">
       <button 
         class="w-1/2 px-4 py-2" 
         @click="bar = 0"
-        :class="bar === 0 ? 'bg-button-2-hover' : ''"
+        :class="bar === 0 ? 'bg-button-1-active' : 'hover:bg-button-1-hover'"
       >Friendlist</button>
       <button 
-        class="w-1/2 px-4 py-2" 
+        class="w-1/2 px-4 py-2"
         @click="bar = 1"
-        :class="bar === 1 ? 'bg-button-2-hover' : ''"
+        :class="bar === 1 ? 'bg-button-1-active' : 'hover:bg-button-1-hover'"
       >Friend requests</button>
     </div>
     <div class="w-full bg-navbar rounded-b-2xl px-4 py-2 shadow">
