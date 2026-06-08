16f48ac22bc465a7970427239bdbd0dd8c2955b0
Sellith <leanevanbree@gmail.com>
2026-06-08 19:59:59 +0200

    Chat Restructuration: Uri changed to be more clear while switching to chats; Addition: added a special chat selection

--- frontend/vue/src/modules/cat_hideout/main.js
+++ frontend/vue/src/modules/cat_hideout/main.js
@@ -3,8 +3,7 @@ import { useThemeStore }  from '@storage/theme';
 import { setupPinia }     from '@shared';
 import BasePage           from '@components/BasePage.vue';
 import Chat               from '@components/Chat.vue'; 
-import App                from './App.vue';
-import SelectChat         from '../../components/SelectChat.vue';
+import App                from './ChatApp.vue';
 
 const app = createApp(App);
 const pinia = setupPinia();
@@ -14,7 +13,6 @@ const savedTheme = useThemeStore();
 document.documentElement.setAttribute("data-theme", savedTheme.current);
 
 app.component('Chat', Chat);
-app.component('SelectChat', SelectChat);
 app.component('BasePage', BasePage);
 
 app.mount('#app')
---
+++ frontend/vue/src/modules/chat/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <BasePage>
+    <SelectChat/>
+  </BasePage>
+</template>
