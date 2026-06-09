c5ea7a4c54048e339583f53e31941ac826744950
Khaïs COLIN <khais.colin@gmail.com>
2026-06-09 15:30:28 +0200

    when creating a new user, also unlock free colors for them

--- backend/back/signals.py
+++ backend/back/signals.py
@@ -1,11 +1,13 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import UserProfile
+from .models import UserProfile, Color
 
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+        free_colors = Color.objects.filter(cost=0)
+        instance.userprofile.unlocked_colors.set(free_colors)
     instance.userprofile.save()
