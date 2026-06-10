ae286c58f285a6bff70b812c6fc0df6ccbf57cdd
Sellith <leanevanbree@gmail.com>
2026-06-10 13:02:24 +0200

    Fix: remove socket error that initialized the current chat room to general (default) before changing to the correct url based chatroom

--- frontend/vue/src/components/Chat.vue
+++ frontend/vue/src/components/Chat.vue
@@ -45,9 +45,6 @@ export default {
   },
   beforeUnmount() {
     if (this.chatSocket) {
-      this.chatSocket.onclose = null;
-      this.chatSocket.onerror = null;
-      this.chatSocket.onmessage = null;
       this.chatSocket.close();
     }
   },
