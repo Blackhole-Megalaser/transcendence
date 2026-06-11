2e5ecf121a8cd1f1c3fd9be66e4d2494dbe7fed4
Sellith <leanevanbree@gmail.com>
2026-06-11 15:20:29 +0200

    Fix: fixed navbar broken profile picture if there is no profile_picture

--- frontend/vue/src/components/ProfileButton.vue
+++ frontend/vue/src/components/ProfileButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button class="size-10 rounded-full overflow-hidden outline-0
     hover:outline-4 outline-navbar-profile-outline duration-150">
-    <img :src="profilePicture !== '' ? profilePicture : defaultCat" alt="profile_pic">
+    <img :src="profilePicture ? profilePicture : defaultCat" alt="profile_pic">
   </button>
 </template>
 
@@ -13,7 +13,7 @@ import defaultCat from '@assets/default_cat.png'
 defineProps({
   profilePicture: {
     type: String,
-    default: ''
+    default: null
   }
 })
 </script>
