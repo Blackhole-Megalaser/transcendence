ec6b4e4e992125eb21d4f9e9715d6d2d8c60dcd3
Sellith <leanevanbree@gmail.com>
2026-06-15 15:38:51 +0200

    Friendlist Update: fixed the broken pfp on small screens; made the text centered; added a pointer cursor on the category button (show friendlist, friendrequests)

--- frontend/vue/src/components/sideProfile/Social.vue
+++ frontend/vue/src/components/sideProfile/Social.vue
@@ -2,12 +2,12 @@
   <div class="w-full relative">
     <div class="w-full h-auto bg-button-1-normal flex justify-center items-stretch rounded-t-2xl overflow-hidden font-semibold text-text-button-1">
       <button 
-        class="w-1/2 px-4 py-2" 
+        class="w-1/2 px-4 py-2 cursor-pointer" 
         @click="bar = 0"
         :class="bar === 0 ? 'bg-button-1-active' : 'hover:bg-button-1-hover'"
       >Friendlist</button>
       <button 
-        class="w-1/2 px-4 py-2"
+        class="w-1/2 px-4 py-2 cursor-pointer"
         @click="bar = 1"
         :class="bar === 1 ? 'bg-button-1-active' : 'hover:bg-button-1-hover'"
       >Friend requests</button>
