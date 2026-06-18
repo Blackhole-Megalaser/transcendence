a08a54ee97608d330bcb5aa5cced596757c096ce
Sellith <leanevanbree@gmail.com>
2026-06-18 17:53:42 +0200

    Update: fix chat gap on the bottom between sm and md

--- frontend/vue/src/modules/chat/App.vue
+++ frontend/vue/src/modules/chat/App.vue
@@ -22,7 +22,7 @@
         <h2 class="text-title h-6 md:h-8 text-xl font-bold my-4">{{ formatChanName(currentChannel) }}</h2>
         <div class="size-10"></div>
       </div>
-      <div class="h-[calc(100dvh-136px)] sm:h-[calc(100dvh-144px)]">
+      <div class="h-[calc(100dvh-136px)] md:h-[calc(100dvh-144px)]">
         <Chat
           :key="currentChannel"
           :initialRoomName="currentChannel"
