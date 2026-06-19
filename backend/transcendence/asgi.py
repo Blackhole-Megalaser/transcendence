0649043a6a0609eae8f54842333953adaf38d332
cczerwin <cczerwin@student.42lehavre.fr>
2026-06-19 13:59:13 +0200

    Moved the ws routing from chat.routing to transcendence.routing to include news apps + base websocket management on those apps

--- backend/transcendence/asgi.py
+++ backend/transcendence/asgi.py
@@ -13,7 +13,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
+from transcendence.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transcendence.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
---
+++ backend/transcendence/routing.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+
+from chat import consumers as chat
+from skribble import consumers as skribble
+from tplace import consumers as tplace
+
+websocket_urlpatterns = [
+    re_path(r"ws/chat/(?P<room_name>[\w-]+)/$", chat.ChatConsumer.as_asgi()),
+    re_path(r"ws/skribble/(?P<room_name>[\w-]+)/$", skribble.ChatConsumer.as_asgi()),
+    re_path(r"ws/tplace/(?P<room_name>[\w-]+)/$", tplace.ChatConsumer.as_asgi()),
+]
