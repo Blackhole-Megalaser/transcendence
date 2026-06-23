3274f13edc3446550047dba1646fc3a930228747
Khaïs COLIN <khais.colin@gmail.com>
2026-06-23 17:26:48 +0200

    remove django nested routers - no longer needed

--- backend/back/urls.py
+++ backend/back/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_nested import routers
+from rest_framework import routers
 from . import views
 
 
