3ef5d094da07518580b0fd73c1fa0977f3e67c06
ael-ghaz <eganas76@gmail.com>
2026-06-29 22:26:05 +0200

    Create Room

--- frontend/vue/src/modules/lobbyskbl/gamerules/GameRules.vue
+++ frontend/vue/src/modules/lobbyskbl/gamerules/GameRules.vue
@@ -1,11 +1,76 @@
 <script setup>
-    import { ref, onMounted, onUnmounted } from 'vue';
+    import { ref, onMounted, onUnmounted, watch } from 'vue';
+    import { getCookie }            from '@shared';
 
-    const roomCode = ref('VWXYZ');
-    const roomName = ref('Amigos');
-    const username = ref('Anas');
+    let roomCode = ref('Default');
+    let roomName = ref('Default');
+    let username = ref('');
+    let users    = ref([]);
 
+    let inputRoomName = ref('');
 
+    onMounted(() => {
+        getUserRoomInfo();
+    });
+
+    onUnmounted(() => {
+        
+    });
+    // watch(message, (newMessage) => {
+
+    // });
+
+    const getUserRoomInfo = async () => {
+        users.value = [];
+        try {
+            const response = await fetch('/api/users/me');
+            const dataUser = await response.json();
+            if (dataUser.skribble) {
+                username.value = dataUser.username;
+                roomCode.value = dataUser.skribble.code;
+                roomName.value = dataUser.skribble.name;
+                dataUser.skribble.players.forEach(element => {
+                    users.value.push(element.username);
+                });
+            }
+        } catch (error) {
+            console.error("Recuperation error :", error);
+        }
+    };
+
+    // const getRoomInfo = async () => {
+    //     try {
+    //         const response = await fetch('/api/skribble/rooms/');
+    //         const dataRooms = await response.json();
+    //         username.value = dataRooms[0].players[0].username;
+    //         roomCode.value = dataRooms[0].code;
+    //         roomName.value = dataRooms[0].name;
+    //     } catch (error) {
+    //         console.error("Recuperation error :", error);
+    //     }
+    // };
+
+    const postCreateRoom = async () => {
+		try {
+			const response = await fetch('/api/skribble/rooms/', {
+				method: 'POST',
+				body: JSON.stringify({
+                    name: inputRoomName.value
+				}),
+				headers: {
+					'X-CSRFToken': getCookie('csrftoken'),
+					'Content-type' : 'application/json'
+				}
+			});
+            if (response.ok) {
+                inputRoomName.value = '';
+                getUserRoomInfo();
+            }
+		} catch (error) {
+			console.error("Recuperation error :", error);
+		}
+	};
+    
 </script>
 
 <template>
@@ -23,8 +88,10 @@
                 <div>
                     <div>Create Room:</div>
                     <div class="flex">
-                        <input/>
-                        <button class="bg-navbar-menu rounded-sm">CREATE ROOM</button>
+                        <input v-model="inputRoomName" placeholder="Write your room name"/>
+                        <button
+                            @click="postCreateRoom"
+                            class="bg-navbar-menu rounded-sm">CREATE ROOM</button>
                     </div>
                 </div>
                 <div>
@@ -49,18 +116,14 @@
                     </div>
                 </div>
                 <div class="text-center">__________ROOM INFO__________</div>
-                <div>Room Code: {{ roomCode }}</div>
-                <div>Room Name: {{ roomName }}</div>
-                <div>Users in lobby:</div>
-                <div class="flex flex-row">
-                    <div>
-                        <div>Avatar1: </div>
-                        <div>Username1: {{ username }}</div>
+                    <div>Room Code: {{ roomCode }}</div>
+                    <div>Room Name: {{ roomName }}</div>
+                    <div>Users in lobby:</div>
+                <div class="flex flex-row gap-4">
+                    <div v-for="(user, index) in users">
+                        <div>Avatar: </div>
+                        <div class="bg-navbar-menu">Username{{ index + 1 }}: {{ user }}</div>
                     </div>
-                    <!-- <div>
-                        <div>Avatar2</div>
-                        <div>Username2</div>
-                    </div> -->
                 </div>
             </div>
         </div>
