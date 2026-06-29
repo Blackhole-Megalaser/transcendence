c2ebb1ae7b07082ff6cbeccc7df03a411a754073
Sellith <leanevanbree@gmail.com>
2026-06-29 17:19:34 +0200

    Update: home page as now content

--- frontend/vue/src/modules/home/main.js
+++ frontend/vue/src/modules/home/main.js
@@ -2,6 +2,7 @@ import { createApp, ref }               from 'vue';
 import { useThemeStore, useUserStore }  from '@storage';
 import { setupPinia }                   from '@shared';
 import BasePage                         from '@components/BasePage.vue';
+import ButtonLogIn                      from '@components/ButtonLogIn.vue';
 import App                              from './HomeApp.vue';
 
 const app = createApp(App);
@@ -12,5 +13,6 @@ const userStore = useUserStore();
 userStore.initUserInfos();
 
 app.component('BasePage', BasePage);
+app.component('ButtonLogin', ButtonLogIn)
 
 app.mount('#app');
