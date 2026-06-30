887442542474aba4fb1d298ec2b8ea5e8357ca1a
Sellith <leanevanbree@gmail.com>
2026-06-30 18:35:35 +0200

    Update: modified pfp hover

--- frontend/vue/src/modules/profile/Profile.vue
+++ frontend/vue/src/modules/profile/Profile.vue
@@ -1,21 +1,36 @@
 <template>
   <section class="w-full flex-center flex-col gap-6 md:gap-10 p-8 pt-12 md:max-xl:pb-0 lg:pt-24!">
-    <div class="w-full grid grid-cols-1 md:grid-cols-2 items-center gap-4">
-      <div class="flex-center flex-col gap-8">
-        <div
-          class="size-24 rounded-full bg-cover bg-center cursor-pointer"
-          :style="{ backgroundImage: `url(${profilePic})` }"
-          @click="$refs.fileInput.click()"
-        />
-        <input ref="fileInput" type="file" accept="image/*" class="hidden" @change="onFileChange">
-        <div v-if="newImage" class="flex items-end justify-center gap-2">
-          <p class="text-text-main/60 w-24 truncate">{{ newImageFile.name }}</p>
-          <Button @click="changeProfilePicture()">Submit</Button>
+    <div class="w-full grid grid-cols-1 md:grid-cols-2 items-center gap-4 lg:gap-12p">
+      <div class="flex justify-center lg:justify-end">
+        <div class="flex-center flex-col gap-8">
+          <div
+            class="group relative size-30 rounded-full overflow-hidden cursor-pointer"
+            @click="$refs.fileInput.click()"
+          >
+            <div 
+              class="absolute inset-0 bg-cover bg-center transition-transform duration-300 group-hover:scale-105"
+              :style="{ backgroundImage: `url(${profilePic})` }"
+            />
+            <div class="absolute inset-0 mix-blend-overlay transition-colors duration-300" />
+            <component 
+              :is="edit" 
+              class="absolute top-1/2 left-1/2 -translate-1/2 size-30 opacity-0 transition-opacity duration-300 group-hover:opacity-100 pointer-events-none"
+            />
+          </div>
+          <input ref="fileInput" type="file" accept="image/*" class="hidden" @change="onFileChange">
+          <div v-if="newImage" class="flex items-end justify-center gap-2">
+            <p class="text-text-main/60 w-32 truncate">{{ newImageFile.name }}</p>
+            <div class="w-28">
+              <Button @click="changeProfilePicture()">Submit</Button>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="flex flex-col justify-center items-center gap-4">
-        <div class="text-title text-4xl font-bold">{{ username }}</div>
-        <p class="text-text-main/60">{{ email }}</p>
+      <div class="flex justify-center lg:justify-start">
+        <div class="flex flex-col justify-center items-center gap-4">
+          <div class="text-title text-4xl font-bold">{{ username }}</div>
+          <p class="text-text-main/60">{{ email }}</p>
+        </div>
       </div>
     </div>
     <hr>
@@ -54,12 +69,14 @@
 </template>
 
 <script setup>
-import { computed, onBeforeMount, onMounted, ref } from 'vue';
-import { storeToRefs }              from 'pinia';
-import { useUserStore }             from '@storage';
-import { getCookie }                from '@shared';
-import Button                       from '@components/Button.vue';
-import defaultCat                   from '@assets/default_cat.png';
+import { computed, onBeforeMount, onMounted, ref }  from 'vue';
+import { storeToRefs }                              from 'pinia';
+
+import { useUserStore } from '@storage';
+import { getCookie }    from '@shared';
+import Button           from '@components/Button.vue';
+import defaultCat       from '@assets/default_cat.png';
+import edit             from '@assets/edit.svg';
 
 const userStore = useUserStore();
 
