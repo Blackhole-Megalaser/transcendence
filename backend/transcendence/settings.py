9f42239d038d08861db334eda2d9b50fd9b13bef
cczerwin <cczerwin@student.42lehavre.fr>
2026-07-01 20:27:44 +0200

    Added a middleware and a last seen field in userProfile for the friendlist online status

--- backend/transcendence/settings.py
+++ backend/transcendence/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "back.middleware.UpdateLastSeenMiddleware",
 ]
 
 ROOT_URLCONF = "transcendence.urls"
